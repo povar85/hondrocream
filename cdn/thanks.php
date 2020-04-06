49ce0047a6ea2a4f26e2ba76453bf7c4915daa0c
povar85 <63042323+povar85@users.noreply.github.com>
2020-04-06 17:25:19 +0400

    ho
    vn

---
+++ cdn/thanks.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html class="no-js" lang="ru">
+<head>
+<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+
+
+    <title>
+Cảm ơn đã đặt hàng! Bác sĩ của chúng tôi sẽ gọi cho bạn sớm!</title>
+    <script>
+  function getUclick_c(){
+    var e=document.cookie.match(new RegExp("(?:^|; )"+"uclick"+"=([^;]*)"));
+    return e?decodeURIComponent(e[1]):void 0
+  }
+  function cnv_pixel(){
+    var e="http://id-healthy.com/",n=document.createElement("img");
+    "function"==typeof window.getUclick_c?n.src=e+"click.php?cnv_id=OPTIONAL&payout=OPTIONAL&uclick="+getUclick_c():n.src=e+"click.php?cnv_id=OPTIONAL&payout=OPTIONAL"
+  }
+  cnv_pixel();
+</script>
+<!-- Facebook Pixel Code -->
+<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=484767862225166&ev=Lead&noscript=1"/>
+<!-- End Facebook Pixel Code -->
+</head>
+<body>
+<script>
+  fbq('track', 'Lead');
+</script>
+
+
+<center><p style="font-weight: bold; font-size: 37px;">
+Cảm ơn đã đặt hàng! Bác sĩ của chúng tôi sẽ gọi cho bạn sớm!</p></center>
+
+
+
+</body>
+</html>
